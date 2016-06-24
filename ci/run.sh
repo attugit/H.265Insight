84dc28e61fcc23089137cd29d45875dba58186d8
Karol Wozniak <karol.wozniak@email.com>
2016-06-24 13:22:10 +0200

    fix run script

--- ci/run.sh
+++ ci/run.sh
@@ -4,15 +4,15 @@
 {
   if [[ "${BUILD_TYPE}" == "RELEASE" ]]; then
     export PATH=$PWD/build/bin:$PATH && \
-    mkdir -p ./data && \
-    mkdir -p ./streams && \
-    wget -q --directory-prefix=./data --accept=zip --input-file=http://wftp3.itu.int/av-arch/jctvc-site/bitstream_exchange/draft_conformance/HEVC_v1 && \
-    find ./data -maxdepth 1 -type f -name '*.zip' | while read f; do unzip -qq -o $f -d ./data/`basename $f .zip`; done && \
-    find ./data -type f -name '*.bit' -or -name '*.bin' | while read f; do sha1sum $f; mv $f ./streams/`sha1sum $f | awk '{print $1}'`.`basename $f`; done && \
+    mkdir -p ./build/data && \
+    mkdir -p ./build/streams && \
+    wget -q --directory-prefix=./build/data --accept=zip --input-file=http://wftp3.itu.int/av-arch/jctvc-site/bitstream_exchange/draft_conformance/HEVC_v1 && \
+    find ./build/data -maxdepth 1 -type f -name '*.zip' | while read f; do unzip -qq -o $f -d ./build/data/`basename $f .zip`; done && \
+    find ./build/data -type f -name '*.bit' -or -name '*.bin' | while read f; do sha1sum $f; mv $f ./build/streams/`sha1sum $f | awk '{print $1}'`.`basename $f`; done && \
     virtualenv tests && \
     source ./tests/bin/activate && \
     pip install pytest && \
-    py.test ../ci/bitstreams.py -v --capture=sys && \
+    py.test ./ci/bitstreams.py -v --capture=sys && \
     deactivate
   fi
 }
